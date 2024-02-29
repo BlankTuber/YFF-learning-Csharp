0599ae48986d5652a2db3b06ef32707fec004527
BlankTuber <elias.neerbye@gmail.com>
2024-02-29 10:43:50 +0100

    Added an infinite loop that forces the user to write something, then it responds back before closing on keypress.

--- LearnignCSharp/Program.cs
+++ LearnignCSharp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 
 namespace HelloWorld
 {
@@ -8,8 +7,20 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("This is how you write to the console!");
+            string cool = CheckForStuff();
+            Console.WriteLine(cool);
+            Console.WriteLine("NIce! It worked. Click any key to close :)");
+            Console.ReadKey();
+        }
 
-            Console.ReadLine();
+        static string CheckForStuff()
+        {
+            string cooltxt = Console.ReadLine();
+            while (string.IsNullOrEmpty(cooltxt))
+            {
+                cooltxt = Console.ReadLine();
+            }
+            return cooltxt;
         }
     }
 }
