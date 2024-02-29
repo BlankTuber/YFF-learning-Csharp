15c5124bb3991c26835207b182db2eeea58803f4
BlankTuber <elias.neerbye@gmail.com>
2024-02-29 10:19:30 +0100

    Add project files.

---
+++ LearnignCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is how you write to the console!");
+
+            Console.ReadLine();
+        }
+    }
+}
